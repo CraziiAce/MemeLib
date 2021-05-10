b008c5098751c02778bfe3627041191c9de5c232
CraziiAce <teddyjraz@gmail.com>
2021-05-10 10:27:33 -0600

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="MemeLib",
-    version="0.1.2",
+    version="0.1.3",
     author="CraziiAce",
     author_email="teddyjraz@gmail.com",
     description="A python lib to get dank memes",
